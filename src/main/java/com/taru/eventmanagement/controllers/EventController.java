3fecafdd5fa9ab05e3f9c36428c813370c270969
Timur Turianytsia <turianytsia.timur@gmail.com>
2023-07-18 17:57:49 +0300

    feat: Added event edit page

--- src/main/java/com/taru/eventmanagement/controllers/EventController.java
+++ src/main/java/com/taru/eventmanagement/controllers/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -30,16 +31,34 @@ public class EventController {
     }
 
     @GetMapping("/event/create")
-    public String createEvent(Model model){
+    public String createEventForm(Model model){
 
         model.addAttribute("event", new EventDTO());
         return "event-create";
     }
 
-    @PostMapping("/event")
+    @PostMapping("/event/create")
     public String createEvent(@ModelAttribute("event") EventDTO event){
 
         eventService.createEvent(event);
+
+        return "redirect:/event";
+    }
+
+    @GetMapping("/event/{eventId}/edit")
+    public String updateEventForm(@PathVariable("eventId") int eventId, Model model){
+
+        EventDTO event = eventService.getEventById(eventId);
+
+        model.addAttribute("event", event);
+        return "event-edit";
+    }
+
+    @PostMapping("/event/{eventId}/edit")
+    public String editEvent(@PathVariable("eventId") int eventId, @ModelAttribute("event") EventDTO event){
+
+        event.setEventId(eventId);
+        eventService.updateEventById(eventId, event);
 
         return "redirect:/event";
     }
